4d828482ea7b43df895b6c773b0da73334267e9c
Andres <2002afan@gmail.com>
2022-02-16 19:09:39 +0100

    Empezado el layout y el codigo para pedir permisos (sin acabar)

---
+++ app/src/main/java/android/ejemplo/atami/Permisos/PermisosMicro.java
@@ -0,0 +1,23 @@
+package android.ejemplo.atami.Permisos;
+
+import android.ejemplo.atami.R;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PermisosMicro extends AppCompatActivity {
+    private Button buttonPermisos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.permisosMicro);
+    }
+
+    public void darPerMicro(View v) {
+
+    }
+
+}
