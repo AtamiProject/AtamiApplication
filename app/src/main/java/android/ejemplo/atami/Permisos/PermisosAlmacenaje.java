0edc795aa0bb1dedf3a0d48893e1db9bd70a21ce
goldensita <claudiafierrez@gmail.com>
2022-02-21 17:07:04 +0100

    permisos almacenaje + letra + imagen logo + color granate

---
+++ app/src/main/java/android/ejemplo/atami/Permisos/PermisosAlmacenaje.java
@@ -0,0 +1,54 @@
+package android.ejemplo.atami.Permisos;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.ejemplo.atami.R;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class PermisosAlmacenaje extends AppCompatActivity {
+
+    private static final int STORAGE_PERMISSION_CODE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.permisos_almacenaje);
+    }
+
+    public void darPermisos(View _) {
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+    }
+
+    public void checkPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(PermisosAlmacenaje.this, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(PermisosAlmacenaje.this, new String[] { permission }, requestCode);
+        } else { //hace falta else?
+            Toast.makeText(PermisosAlmacenaje.this, "Permission already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(PermisosAlmacenaje.this, "Storage Permission Granted", Toast.LENGTH_SHORT).show();
+                //MOVER SIGUIENTE ACTIVITY
+            } else {
+                Toast.makeText(PermisosAlmacenaje.this, "Storage Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+}
