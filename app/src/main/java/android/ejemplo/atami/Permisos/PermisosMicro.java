1304a25aa0069c92a119943b1e0829647154c654
Andres <2002afan@gmail.com>
2022-02-22 15:11:24 +0100

    Modificados archivos para no generar conflicto (permisos_micro.xml)

--- app/src/main/java/android/ejemplo/atami/Permisos/PermisosMicro.java
+++ app/src/main/java/android/ejemplo/atami/Permisos/PermisosMicro.java
@@ -13,7 +13,7 @@ public class PermisosMicro extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.permisosMicro);
+        setContentView(R.layout.permisos_micro);
     }
 
     public void darPerMicro(View v) {
