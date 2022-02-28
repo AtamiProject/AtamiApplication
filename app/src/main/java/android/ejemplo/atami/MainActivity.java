0ed12fc7e2cbead51a2a4485e80b2687014831f6
goldensita <claudiafierrez@gmail.com>
2022-02-28 23:19:41 +0100

    menu

--- app/src/main/java/android/ejemplo/atami/MainActivity.java
+++ app/src/main/java/android/ejemplo/atami/MainActivity.java
@@ -1,14 +1,47 @@
 package android.ejemplo.atami;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.ejemplo.atami.Permisos.PermisosAlmacenaje;
+import android.ejemplo.atami.principal.PantallaPrincipal;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Intent intent;
+    Button button;
+    private static final int STORAGE_PERMISSION_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
     }
+
+    public void next(View _) {
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
+        //intent = new Intent(this, PermisosAlmacenaje.class);
+        //startActivity(intent);
+    }
+
+    public void checkPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_DENIED) {
+            intent = new Intent(this, PermisosAlmacenaje.class);
+            startActivity(intent);
+        } else {
+            intent = new Intent(this, PantallaPrincipal.class);
+            startActivity(intent);
+        }
+    }
+
 }
---
+++ app/src/main/java/android/ejemplo/atami/principal/PantallaPrincipal.java
@@ -0,0 +1,59 @@
+package android.ejemplo.atami.principal;
+
+import android.ejemplo.atami.R;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class PantallaPrincipal extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantalla_principal);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void mostrarMenu(View _) {
+        if(!drawerLayout.isDrawerOpen(Gravity.RIGHT)) {
+            drawerLayout.openDrawer(Gravity.LEFT);
+        } else {
+            drawerLayout.closeDrawer(Gravity.RIGHT);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(Gravity.LEFT)){
+            drawerLayout.closeDrawer(Gravity.LEFT);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    //https://material.io/components/buttons-floating-action-button/android#regular-fabs
+    //https://maven.google.com/web/index.html#
+    //https://stackoverflow.com/questions/22530394/how-to-open-sliding-menu-on-buttons-click-event
+    //https://www.youtube.com/watch?v=63Ipzp9U_bU
+    //https://www.youtube.com/watch?v=do4vb0MdLFY ->slidemenu
+}
